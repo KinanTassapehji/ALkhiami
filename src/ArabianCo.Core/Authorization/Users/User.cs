0d91a75c3fee8378769f766609b231853bf8ad81
Malaz Tassapehji <malaz.tassapehji@gmail.com>
2025-09-03 09:50:58 +0300

    review user entity

--- src/ArabianCo.Core/Authorization/Users/User.cs
+++ src/ArabianCo.Core/Authorization/Users/User.cs
@@ -8,13 +8,15 @@ using ArabianCo.Domain.Orders;
 
 namespace ArabianCo.Authorization.Users
 {
-    public class User : AbpUser<User>
-    {
-        public const string DefaultPassword = "123qwe";
-        public static string CreateRandomPassword()
-        {
-            return Guid.NewGuid().ToString("N").Truncate(16);
-        }
+	public class User : AbpUser<User>
+	{
+		public const string DefaultPassword = "123qwe";
+
+		public static string CreateRandomPassword()
+			=> Guid.NewGuid().ToString("N").Truncate(16);
+
+		// Navigation: a user can have many orders
+		public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 
 		// (Optional, existing template method – safe to keep even if not using tenants)
 		public static User CreateTenantAdminUser(int tenantId, string emailAddress)
